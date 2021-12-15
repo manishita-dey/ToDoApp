658838e61549f10c25b611cec6b714f0e149908c
MANISHITA DEY <deymani2752.9119@gmail.com>
2021-12-15 19:21:28 +0530

    First Update

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
 
 # CONNECTING TO DATABASE
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
